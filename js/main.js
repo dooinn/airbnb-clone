47efde8e6cf9aeec62c67ebad7a9b8373e242608
dooinn <kimdooinn1@gmail.com>
2020-12-07 15:33:10 +0900

    simple edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-// Selectors
+
+// Header scroll animation
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
 
 
+//user menu box
+function openUser() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-// Eventlisteners
-
-
-
+window.onclick = function (event) {
+    if (!event.target.matches('.user')) {
+        var dropdowns = document.getElementsByClassName("dropdown__content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dorpdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
